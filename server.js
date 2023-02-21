adcd6a69d08dedad51b7510a3e0880f01e2c551c
Efrain Cataño <efrain.catano97@gmail.com>
2023-02-21 20:50:07 -0300

    Changing server url

--- server.js
+++ server.js
@@ -8,6 +8,6 @@ function website(req, res){
 let server = http.createServer(website);
 
 
-server.listen(8080, "127.0.0.1");
+server.listen(8080, "https://efrain-w1-a1.onrender.com");
 
 console.log("Server listening on 8080");
