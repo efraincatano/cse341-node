f7d9f917f331e35cc3db7fc19218e9e9677853a5
Efrain Cataño <efrain.catano97@gmail.com>
2023-02-21 20:38:02 -0300

    second commit

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const http = require("http");
 
 function website(req, res){
     res.writeHead(200, {"Content-type": "text/plain"});
-    res.end("Efrain Pescador");
+    res.end("Efrain Cataño Pescador");
 }
 
 let server = http.createServer(website);
 
 
 server.listen(8080, "127.0.0.1");
+
+console.log("Server listening on 8080");
