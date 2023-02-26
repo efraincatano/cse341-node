12d581bb97756ff2c23a4bab0b8e3d39243b3cc1
Efrain Cataño <efrain.catano97@gmail.com>
2023-02-26 12:36:24 -0300

    api working

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -2,7 +2,7 @@ const mongodb = require('../db/connect');
 const ObjectId = require('mongodb').ObjectId;
 
 const getAllContants = async (req, res, next) => {
-  const result = await mongodb.getDb().db().collection('node_app.contacts').find();
+  const result = await mongodb.getDb().db("node_app").collection('contacts').find();
   result.toArray().then((lists) => {
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json(lists);
@@ -13,8 +13,8 @@ const getOneContant = async (req, res, next) => {
   const userId = new ObjectId(req.params.id);
   const result = await mongodb
     .getDb()
-    .db()
-    .collection('node_app.contacts')
+    .db("node_app")
+    .collection('contacts')
     .find({ _id: userId });
   result.toArray().then((lists) => {
     res.setHeader('Content-Type', 'application/json');
